c92709d0c2eb6ee1fb427046862420666f91f861
SharmaYojana <joy.dfm@gmail.com>
2016-12-10 17:21:24 -0500

    Running Recyclerview

--- app/src/main/java/nyc/c4q/yojana/practicaltestyojana/RecordsAdapter.java
+++ app/src/main/java/nyc/c4q/yojana/practicaltestyojana/RecordsAdapter.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import java.util.List;
 
 import nyc.c4q.yojana.practicaltestyojana.Pojo.Data;
+import nyc.c4q.yojana.practicaltestyojana.Pojo.Record;
 
 /**
  * Created by yojanasharma on 12/10/16.
@@ -15,9 +16,13 @@ import nyc.c4q.yojana.practicaltestyojana.Pojo.Data;
 public class RecordsAdapter extends RecyclerView.Adapter{
 private Context context;
     private List<Data> list;
+    private List<Record> recordList;
+
     public RecordsAdapter(List<Data> list){
         this.list = list;
-    }
+
+
+        }
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return new RecordsViewHolder(parent);
