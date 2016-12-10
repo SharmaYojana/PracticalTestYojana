038b02f92971264cc5e5d56d236326577e09dc6c
SharmaYojana <joy.dfm@gmail.com>
2016-12-10 15:53:25 -0500

    Retrofit Works

--- app/src/main/java/nyc/c4q/yojana/practicaltestyojana/MainActivity.java
+++ app/src/main/java/nyc/c4q/yojana/practicaltestyojana/MainActivity.java
@@ -1,13 +1,48 @@
 package nyc.c4q.yojana.practicaltestyojana;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.util.List;
+
+import nyc.c4q.yojana.practicaltestyojana.Pojo.Data;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+    final static String URL = "http://vine.co";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(URL)
+                .addConverterFactory(GsonConverterFactory.create()).build();
+
+        RestApi service = retrofit.create(RestApi.class);
+        Call<List<Data>> call = service.getTheListOfRecords();
+
+        call.enqueue(new Callback<List<Data>>() {
+            @Override
+            public void onResponse(Call<List<Data>> call, Response<List<Data>> response) {
+                List<Data> list = response.body();
+                Log.d("Retrofit success", String.valueOf(list.get(0).getRecords()));
+//                recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+//                recyclerView.setAdapter(new CountryAdapter(list));
+            }
+
+            @Override
+            public void onFailure(Call<List<Data>> call, Throwable t) {
+                Log.d("Retrofit Failure", t.toString());
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/nyc/c4q/yojana/practicaltestyojana/RestApi.java
@@ -0,0 +1,17 @@
+package nyc.c4q.yojana.practicaltestyojana;
+
+import java.util.List;
+
+import nyc.c4q.yojana.practicaltestyojana.Pojo.Data;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Created by yojanasharma on 12/10/16.
+ */
+
+public interface RestApi {
+    @GET("api/timelines/users/918753190470619136")
+    Call<List<Data>> getTheListOfRecords();
+}
+
